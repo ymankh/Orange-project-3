fc9ce993fae5ffc7a098a70cc105b1f6bb8e95ab
Yaman Khashashneh <ymankh1997@gmail.com>
2024-06-20 14:32:24 +0300

    start with the leave application

--- assets/javascripts/create_new_task.js
+++ assets/javascripts/create_new_task.js
@@ -1,13 +1,40 @@
-let SupervisorSelect = document.getElementById("SupervisorSelect");
+const url = "assets/data/employees_data.json";
 
-async function getData() {
-  const response = await fetch("assets/data/employees_data.json");
-  const employees = await response.json();
-  for (const employee of employees) {
-    let NameOption = document.createElement("option");
-    NameOption.innerText = employee["Full Name"];
-    NameOption.value = employee["Employee ID"];
-    SupervisorSelect.appendChild(NameOption);
-  }
+async function getData(selectElement) {
+  const response = await fetch(url);
+  const jsonData = await response.json();
+  jsonData.forEach((employee) => {
+    const option = document.createElement("option");
+    option.value = employee["Full Name"];
+    option.textContent = employee["Full Name"];
+    selectElement.appendChild(option);
+  });
 }
-getData();
+
+function addMemberDropdown(selectElement) {
+  console.log(selectElement);
+  const membersContainer = document.getElementById("membersContainer");
+  const newSelect = selectElement.cloneNode(true);
+  newSelect.value = "+ Add person"; // Reset the new dropdown
+  newSelect.onchange = function () {
+    addMemberDropdown(newSelect);
+  };
+  membersContainer.appendChild(newSelect);
+  getData(newSelect); // Populate new dropdown
+  styleDropdown(selectElement); // Style the changed dropdown
+}
+function styleDropdown(selectElement) {
+  selectElement.style.backgroundColor = "#93c9de";
+  selectElement.style.color = "black";
+  selectElement.style.border = "none";
+  selectElement.style.borderRadius = "20px";
+  selectElement.style.margin = "5px";
+}
+
+// Initialize the first dropdowns
+getData(document.getElementById("AssignMembersSelect"));
+
+function addTaskToLocalStorage() {
+  tasks = (localStorage.tasks && JSON.parse(localStorage.tasks)) || [];
+  tasks.push()
+}
---
+++ assets/javascripts/leave_aplications.js
@@ -0,0 +1,36 @@
+const url = "assets/data/employees_data.json";
+const employeeNameSelect = document.getElementById("employee-name");
+const jopTitle = document.getElementById("jop-title");
+const department = document.getElementById("department");
+const startDate = document.getElementById("startDate");
+startDate.value = new Date().toISOString().split("T")[0];
+let employeeData;
+async function addEmployeeNamesOptions() {
+  if (!employeeData) {
+    const response = await fetch(url);
+    employeeData = await response.json();
+  }
+  employeeData.forEach((employee) => {
+    const option = document.createElement("option");
+    option.value = employee["Employee ID"];
+    option.textContent = employee["Full Name"];
+    employeeNameSelect.appendChild(option);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await addEmployeeNamesOptions();
+  const choices = new Choices(employeeNameSelect, {
+    searchEnabled: true,
+    placeholderValue: "Select Employee",
+    shouldSort: false,
+  });
+});
+
+employeeNameSelect.addEventListener("change", () => {
+  const employee = employeeData.find(
+    (employee) => employee["Employee ID"] === employeeNameSelect.value
+  );
+  department.value = employee["Department"];
+  jopTitle.value = employee["Job Title"];
+});
